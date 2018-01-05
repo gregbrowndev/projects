3f6f60b98d2dc0664273c1ee0ffa4b259150cafc
Greg Brown <gregb43@googlemail.com>
2018-01-05 20:10:48 +0000

    Added alternative ways to return adjacent elements in React
    Aadjacent elements means the render function doesn't return a single parent element. E.g. an array of JSX is one way to return adjacent elements, or by wrapping them up into a HOC. This commit adds two files 'WithClass.js' and 'withClass2' which are alternative ways to create a parameterised HOC. The 'Aux.js' HOC is simpler but not paramterised. In newer versions of React, the Aux component is redundant, use 'fragments' i.e. render(<>Content</>)

--- section3_basics/src/components/Persons/Person/Person.js
+++ section3_basics/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './Person.css';
 
@@ -7,7 +8,12 @@ const person = (props) => {
     <div className={styles.Person}>
       <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}></input>
+      <input type="text"
+             ref={(inp) => {this.inputElement = inp}}
+             onChange={props.changed}
+             value={props.name}
+      >
+      </input>
     </div>
   );
 
@@ -16,6 +22,13 @@ const person = (props) => {
   //     <p key="2">{props.children}</p>,
   //     <input key="3" type="text" onChange={props.changed} value={props.name}></input>,
   // ];
+};
+
+person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func,
 };
 
 export default person;
