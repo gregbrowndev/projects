7c00ee12c1b6fe6a5c49107411551e381790a53c
Unknown <gregb43@googlemail.com>
2018-01-09 23:11:24 +0000

    Add tab-based screen with icons
    Added FindPlaceScreen and SharePlaceScreen components. These are rendered within a tab-based screen. We use the Ionicons icons to give our tabs an icon. Note for Android this is mandatory!

---
+++ complete_guide/basics/src/screens/FindPlace/FindPlace.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class FindPlaceScreen extends Component {
+
+  render() {
+    return (
+      <View>
+        <Text>On ShareFindScreen</Text>
+      </View>
+    )
+  }
+}
+
+export default FindPlaceScreen;
