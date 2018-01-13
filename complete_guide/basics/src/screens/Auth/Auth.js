e380d5ce7280e47b21f538f2ddb8c9d6f14ed63a
Unknown <gregb43@googlemail.com>
2018-01-13 00:02:30 +0000

    Make Auth screen responsive
    Updated Auth screen to adjust the content and styling dynamically to the device dimensions and orientation.

--- complete_guide/basics/src/screens/Auth/Auth.js
+++ complete_guide/basics/src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, Button, TextInput, StyleSheet, ImageBackground} from 'react-native';
+import {View, Text, Button, TextInput, StyleSheet, ImageBackground, Dimensions} from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
@@ -9,22 +9,67 @@ import backgroundImage from '../../assets/background.jpg';
 import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground';
 
 class AuthScreen extends Component {
+  state = {
+    styles: {
+      pwContainerFlexDirection: "column",
+      pwContainerJustifyContent: "flex-start",
+      pwWrapperWidth: "100%"
+    }
+  };
+
+  constructor(props) {
+    super(props);
+    Dimensions.addEventListener("change", (dims) => {
+      this.setState({
+        styles: {
+          pwContainerFlexDirection:
+            Dimensions.get('window').height > 500 ? "column" : "row",
+          pwContainerJustifyContent:
+            Dimensions.get('window').height > 500 ? "flex-start" : "space-between",
+          pwWrapperWidth:
+            Dimensions.get('window').height > 500 ? "100%" : "45%"
+        }
+      });
+    });
+  }
+
   loginHandler = () => {
     startMainTabs();
   };
 
   render() {
+    let headingText = null;
+
+    if (Dimensions.get('window').height > 500) {
+      headingText = (
+        <MainText>
+          <HeadingText>Please Login</HeadingText>
+        </MainText>
+      );
+    }
+
     return (
       <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
         <View style={styles.container}>
-          <MainText>
-            <HeadingText>Please Login</HeadingText>
-          </MainText>
+          {headingText}
           <ButtonWithBackground backgroundColor="#29aaf4">Switch to Login</ButtonWithBackground>
           <View style={styles.inputContainer}>
             <DefaultInput placeholder="Your Email Address" style={styles.input}/>
-            <DefaultInput placeholder="Password" style={styles.input}/>
-            <DefaultInput placeholder="Confirm Password" style={styles.input}/>
+            <View style={{
+              flexDirection: this.state.styles.pwContainerFlexDirection,
+              justifyContent: this.state.styles.pwContainerJustifyContent
+            }}>
+              <View style={{
+                width: this.state.styles.pwWrapperWidth
+              }}>
+                <DefaultInput placeholder="Password" style={styles.input}/>
+              </View>
+              <View style={{
+                width: this.state.styles.pwWrapperWidth
+              }}>
+                <DefaultInput placeholder="Confirm Password" style={styles.input}/>
+              </View>
+            </View>
           </View>
           <ButtonWithBackground backgroundColor="#29aaf4" onPress={this.loginHandler}>Submit</ButtonWithBackground>
         </View>
@@ -49,6 +94,13 @@ const styles = StyleSheet.create({
   backgroundImage: {
     width: "100%",
     flex: 1
+  },
+  passwordContainer: {
+    flexDirection: Dimensions.get('window').height > 500 ? "column" : "row",
+    justifyContent: "space-between"
+  },
+  passwordWrapper: {
+    width: Dimensions.get('window').height > 500 ? "100%" : "45%"
   }
 });
 
