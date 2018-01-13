b154c03c615e0b7c920d953e55076c747f3586c9
Unknown <gregb43@googlemail.com>
2018-01-13 01:09:35 +0000

    Do assignment 2 - style PlaceDetail screen
    Made PlaceDetail a little bit for responsive by shrinking the image and re-centering vertically when the screen size is below the threshold.

--- complete_guide/basics/src/screens/PlaceDetail/PlaceDetail.js
+++ complete_guide/basics/src/screens/PlaceDetail/PlaceDetail.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
-import {Platform, View, Image, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {Platform, View, Image, Text, StyleSheet, TouchableOpacity, ScrollView, Dimensions} from 'react-native';
 import {connect} from 'react-redux';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 import {deletePlace} from '../../store/actions';
 
 class PlaceDetail extends Component {
+  state = {
+    viewMode: Dimensions.get('window').height > 500 ? "portrait" : "landscape"
+  };
+
+  constructor(props) {
+    super(props);
+    Dimensions.addEventListener("change", this.updateViewMode);
+  }
+
+  componentWillUnmount() {
+    Dimensions.removeEventListener("change", this.updateViewMode)
+  }
+
+  updateViewMode = dims => {
+    this.setState({
+      viewMode: dims.window.height > 500 ? "portrait" : "landscape"
+    });
+
+    console.log('updateViewMode', this.state.viewMode);
+    console.log('height', dims.height);
+  };
+
   placeDeletedHandler = () => {
     this.props.onDeletePlace(this.props.selectedPlace.key);
     this.props.navigator.pop();
@@ -13,22 +35,28 @@ class PlaceDetail extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <View>
-          <Image
-            source={this.props.selectedPlace.image}
-            style={styles.placeImage}
-          />
-          <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.headerContainer}>
+            <Image
+              source={this.props.selectedPlace.image}
+              style={
+                this.state.viewMode === "portrait"
+                  ? styles.portraitPlaceImage
+                  : styles.landscapePlaceImage
+              }
+            />
+            <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
+          </View>
+          <View>
+            <TouchableOpacity onPress={this.placeDeletedHandler}>
+              <View style={styles.deleteButton}>
+                <Icon size={30} name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'} color="red"/>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View>
-          <TouchableOpacity onPress={this.placeDeletedHandler}>
-            <View style={styles.deleteButton}>
-              <Icon size={30} name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'} color="red"/>
-            </View>
-          </TouchableOpacity>
-        </View>
-      </View>
+      </ScrollView>
     )
   }
 }
@@ -37,9 +65,16 @@ const styles = StyleSheet.create({
   container: {
     margin: 22
   },
-  placeImage: {
+  headerContainer: {
+    alignItems: 'center'
+  },
+  portraitPlaceImage: {
     width: "100%",
     height: 200
+  },
+  landscapePlaceImage: {
+    width: "60%",
+    height: 160
   },
   placeName: {
     fontWeight: "bold",
