6cab9653648c85f41321071e0a8f77334499c9b6
Unknown <gregb43@googlemail.com>
2018-01-13 00:25:30 +0000

    Unregister from Event Listener to prevent memory leak

--- complete_guide/basics/src/screens/Auth/Auth.js
+++ complete_guide/basics/src/screens/Auth/Auth.js
@@ -15,12 +15,18 @@ class AuthScreen extends Component {
 
   constructor(props) {
     super(props);
-    Dimensions.addEventListener("change", (dims) => {
-      this.setState({
-        viewMode: Dimensions.get('window').height > 500 ? "portrait" : "landscape"
-      });
-    });
+    Dimensions.addEventListener("change", this.updateStyles);
   }
+
+  componentWillUnmount() {
+    Dimensions.removeEventListener("change", this.updateStyles)
+  }
+
+  updateStyles = dims => {
+    this.setState({
+      viewMode: dims.window.height > 500 ? "portrait" : "landscape"
+    });
+  };
 
   loginHandler = () => {
     startMainTabs();
