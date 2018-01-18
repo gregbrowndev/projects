722498aa4b4907d03dbbd8d3f11f7ac1a47b84ec
Unknown <gregb43@googlemail.com>
2018-01-18 23:54:45 +0000

    Install react-native-image-picker
    Added the image picker library and configure it for Android

--- complete_guide/basics/android/app/src/main/java/com/basics/MainApplication.java
+++ complete_guide/basics/android/app/src/main/java/com/basics/MainApplication.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.imagepicker.ImagePickerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,7 +22,8 @@ public class MainApplication extends NavigationApplication {
     // No need to add RnnPackage and MainReactPackage
     return Arrays.<ReactPackage>asList(
         new VectorIconsPackage(),
-        new MapsPackage()
+        new MapsPackage(),
+        new ImagePickerPackage()
     );
   }
 
