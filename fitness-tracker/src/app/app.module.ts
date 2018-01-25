3727d60c5034257501200afb3a5e444b692df137
Unknown <gregb43@googlemail.com>
2018-01-25 23:26:06 +0000

    Add hints and error message to form controls

--- fitness-tracker/src/app/app.module.ts
+++ fitness-tracker/src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {MaterialModule} from './material/material.module';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { TrainingComponent } from './training/training.component';
-import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { PastTrainingComponent } from './training/past-training/past-training.component';
-import { HomeComponent } from './home/home.component';
+import {LoginComponent} from './auth/login/login.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {TrainingComponent} from './training/training.component';
+import {NewTrainingComponent} from './training/new-training/new-training.component';
+import {CurrentTrainingComponent} from './training/current-training/current-training.component';
+import {PastTrainingComponent} from './training/past-training/past-training.component';
+import {HomeComponent} from './home/home.component';
 import {AppRoutingModule} from './app-routing.module';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -28,8 +30,10 @@ import {AppRoutingModule} from './app-routing.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MaterialModule,
-    AppRoutingModule
+    FlexLayoutModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
