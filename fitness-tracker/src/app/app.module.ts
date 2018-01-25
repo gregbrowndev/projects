b9fd797b9ca7d32ac92bc5570513c6292c3ed4d2
Unknown <gregb43@googlemail.com>
2018-01-25 22:47:59 +0000

    Add basic components and routing

--- fitness-tracker/src/app/app.module.ts
+++ fitness-tracker/src/app/app.module.ts
@@ -4,16 +4,32 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import {MaterialModule} from './material/material.module';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { PastTrainingComponent } from './training/past-training/past-training.component';
+import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    TrainingComponent,
+    NewTrainingComponent,
+    CurrentTrainingComponent,
+    PastTrainingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
