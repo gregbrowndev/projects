ce494b3c2e1f69db76ae73502cf746ee8e88b3bf
Unknown <gregb43@googlemail.com>
2018-03-16 22:51:25 +0000

    Use 'addAll' to cache multiple resources

--- pwas/pwa-project/public/sw.js
+++ pwas/pwa-project/public/sw.js
@@ -1,22 +1,32 @@
 self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker...', event);
+  console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
     caches.open('static')
       .then(function(cache) {
         console.log('[Service Worker] Precaching App Shell');
-        cache.add('/');
-        cache.add('/index.html');
-        cache.add('/src/js/app.js');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/js/app.js',
+          '/src/js/feed.js',
+          '/src/js/material.min.js',
+          '/src/css/app.css',
+          '/src/css/feed.css',
+          '/src/images/main-image.jpg',
+          'https://fonts.googleapis.com/css?family=Roboto:400,700',
+          'https://fonts.googleapis.com/icon?family=Material+Icons',
+          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        ]);
       })
   )
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker...', event);
+  console.log('[Service Worker] Activating Service Worker ....', event);
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event){
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
