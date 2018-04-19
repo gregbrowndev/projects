5539a0fd050ca2b6f420505d17b0c3a913ce0b22
Unknown <gregb43@googlemail.com>
2018-04-19 22:26:06 +0100

    Add sign up and sign in to Cognito
    Updated the AuthService to connect with Cognito. This allows a user to sign up and verify their account with the verification code sent via email. The user can then sign in with their username/email and password.

--- compare-yourself/src/app/user/auth.service.ts
+++ compare-yourself/src/app/user/auth.service.ts
@@ -5,14 +5,26 @@ import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
+import {AuthenticationDetails, CognitoUser, CognitoUserAttribute, CognitoUserPool, CognitoUserSession} from 'amazon-cognito-identity-js';
+
 import { User } from './user.model';
+
+const POOL_DATA = {
+  UserPoolId: 'eu-west-2_uceBeIcP2',
+  ClientId: '509or6d6fd3saqfuuu6ftmsb4b'
+};
+
+const userPool = new CognitoUserPool(POOL_DATA);
 
 @Injectable()
 export class AuthService {
   authIsLoading = new BehaviorSubject<boolean>(false);
   authDidFail = new BehaviorSubject<boolean>(false);
   authStatusChanged = new Subject<boolean>();
+  registeredUser: CognitoUser;
+
   constructor(private router: Router) {}
+
   signUp(username: string, email: string, password: string): void {
     this.authIsLoading.next(true);
     const user: User = {
@@ -20,32 +32,83 @@ export class AuthService {
       email: email,
       password: password
     };
+    const attrbList: CognitoUserAttribute[] = [];
     const emailAttribute = {
       Name: 'email',
       Value: user.email
     };
+    attrbList.push( new CognitoUserAttribute(emailAttribute));
+    userPool.signUp(user.username, user.password, attrbList, null,  (err, result) => {
+      if (err) {
+        this.authDidFail.next(true);
+        this.authIsLoading.next(false);
+        return;
+      } else {
+        this.authDidFail.next(false);
+        this.authIsLoading.next(false);
+        this.registeredUser = result.user;
+      }
+    });
     return;
   }
+
   confirmUser(username: string, code: string) {
     this.authIsLoading.next(true);
     const userData = {
       Username: username,
+      Pool: userPool
     };
+    const cognitoUser = new CognitoUser(userData);
+    cognitoUser.confirmRegistration(code, true, (err, result) => {
+      if (err) {
+        this.authDidFail.next(true);
+        this.authIsLoading.next(false);
+        return;
+      } else {
+        this.authDidFail.next(false);
+        this.authIsLoading.next(false);
+        this.router.navigate(['/']);
+      }
+    });
   }
+
   signIn(username: string, password: string): void {
     this.authIsLoading.next(true);
     const authData = {
       Username: username,
       Password: password
     };
+    const authDetails = new AuthenticationDetails(authData);
+    const userData = {
+      Username: username,
+      Pool: userPool
+    };
+    const cognitoUser = new CognitoUser(userData);
+    const that = this;
+    cognitoUser.authenticateUser(authDetails, {
+      onSuccess (result: CognitoUserSession) {
+        that.authStatusChanged.next(true);
+        that.authDidFail.next(false);
+        that.authIsLoading.next(false);
+        console.log(result);
+      },
+      onFailure (err) {
+        that.authDidFail.next(true);
+        that.authIsLoading.next(false);
+        console.log(err);
+      }
+    });
     this.authStatusChanged.next(true);
     return;
   }
+
   getAuthenticatedUser() {
   }
+
   logout() {
     this.authStatusChanged.next(false);
   }
+
   isAuthenticated(): Observable<boolean> {
     const user = this.getAuthenticatedUser();
     const obs = Observable.create((observer) => {
@@ -58,6 +121,7 @@ export class AuthService {
     });
     return obs;
   }
+
   initAuth() {
     this.isAuthenticated().subscribe(
       (auth) => this.authStatusChanged.next(auth)
