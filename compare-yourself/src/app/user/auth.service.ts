7843ee9575b318c9118967e6c05219445ca6c5fe
Unknown <gregb43@googlemail.com>
2018-04-19 22:32:01 +0100

    Get authentication state and logout

--- compare-yourself/src/app/user/auth.service.ts
+++ compare-yourself/src/app/user/auth.service.ts
@@ -103,9 +103,11 @@ export class AuthService {
   }
 
   getAuthenticatedUser() {
+    return userPool.getCurrentUser();
   }
 
   logout() {
+    this.getAuthenticatedUser().signOut();
     this.authStatusChanged.next(false);
   }
 
@@ -115,7 +117,17 @@ export class AuthService {
       if (!user) {
         observer.next(false);
       } else {
-        observer.next(false);
+        user.getSession((err, session) => {
+          if (err) {
+            observer.next(false);
+          } else {
+            if (session.isValid()) {
+              observer.next(true);
+            } else {
+              observer.next(false);
+            }
+          }
+        });
       }
       observer.complete();
     });
