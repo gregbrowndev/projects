e30c652ffb8fe6882a7536583110c048566d4fbd
Unknown <gregb43@googlemail.com>
2018-04-22 21:38:55 +0100

    Add Chatty app project files

---
+++ chatty/server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
+import {makeExecutableSchema, addMockFunctionsToSchema} from 'graphql-tools';
+import bodyParser from 'body-parser';
+import {createServer} from 'http';
+import {Schema} from './data/schema';
+import {Mocks} from './data/mocks';
+
+const GRAPHQL_PORT = 8080;
+const app = express();
+const executableSchema = makeExecutableSchema({
+    typeDefs: Schema,
+});
+
+addMockFunctionsToSchema({
+    schema: executableSchema,
+    mocks: Mocks,
+    preserveResolvers: true,
+});
+
+// `context` must be an object and can't be undefined when using connectors
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+    schema: executableSchema,
+    context: {}, // at least(!) an empty object
+}));
+
+app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+}));
+
+const graphQLServer = createServer(app);
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(`GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`));
