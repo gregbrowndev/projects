a639e3467c5609db785d391071a6ef109f50f044
Unknown <gregb43@googlemail.com>
2018-04-24 23:03:12 +0100

    Add basic navigation to RN app
    Added basic tab-based navigation using react-navigation to the app with place holder components.

--- chatty/client/App.js
+++ chatty/client/App.js
@@ -17,20 +17,28 @@ import { ApolloLink } from 'apollo-link';
 import { ApolloProvider } from 'react-apollo';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createHttpLink } from 'apollo-link-http';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { ReduxCache, apolloReducer } from 'apollo-cache-redux';
 import ReduxLink from 'apollo-link-redux';
 import { onError } from 'apollo-link-error';
+
+import AppWithNavigationState, {
+  navigationReducer,
+  navigationMiddleware,
+} from './navigation';
 
 const URL = 'localhost:8080'; // set your comp's url here
 
 const store = createStore(
   combineReducers({
     apollo: apolloReducer,
+    nav: navigationReducer
   }),
   {}, // initial state
-  composeWithDevTools(),
+  composeWithDevTools(
+    applyMiddleware(navigationMiddleware),
+  ),
 );
 
 const cache = new ReduxCache({store});
@@ -57,32 +65,10 @@ export default class App extends Component<Props> {
     return (
       <ApolloProvider client={client}>
         <Provider store={store}>
-          <View style={styles.container}>
-            <Text style={styles.welcome}>
-              Welcome to React Native!
-            </Text>
-          </View>
+          <AppWithNavigationState/>
         </Provider>
       </ApolloProvider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
