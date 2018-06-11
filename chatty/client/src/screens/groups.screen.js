54735a31dd40e9276c6632b71f1d75f1dc1c634e
Unknown <gregb43@googlemail.com>
2018-06-11 22:59:50 +0100

    Refactor groups screen into separate component

--- chatty/client/src/screens/groups.screen.js
+++ chatty/client/src/screens/groups.screen.js
@@ -1,68 +1,21 @@
 import { _ } from 'lodash';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import {
-  FlatList,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-} from 'react-native';
+import { FlatList, StyleSheet, View, } from 'react-native';
+import Group from "../components/group.component";
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
     flex: 1,
-  },
-  groupContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    borderBottomColor: '#eee',
-    borderBottomWidth: 1,
-    paddingHorizontal: 12,
-    paddingVertical: 8,
-  },
-  groupName: {
-    fontWeight: 'bold',
-    flex: 0.7,
-  },
+  }
 });
+
 // create fake data to populate our ListView
 const fakeData = () => _.times(100, i => ({
   id: i,
   name: `Group ${i}`,
 }));
-
-class Group extends Component {
-  constructor(props) {
-    super(props);
-    this.goToMessages = this.props.goToMessages.bind(this, this.props.group);
-  }
-
-  render() {
-    const { id, name } = this.props.group;
-    return (
-      <TouchableHighlight
-        key={id}
-        onPress={this.goToMessages}
-      >
-        <View style={styles.groupContainer}>
-          <Text style={styles.groupName}>{`${name}`}</Text>
-        </View>
-      </TouchableHighlight>
-    );
-  }
-}
-
-Group.propTypes = {
-  goToMessages: PropTypes.func.isRequired,
-  group: PropTypes.shape({
-    id: PropTypes.number,
-    name: PropTypes.string,
-  }),
-};
 
 class Groups extends Component {
   static navigationOptions = {
