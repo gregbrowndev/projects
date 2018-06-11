052f029c8ed9666114f281eaa47e140a42ee0cf9
Unknown <gregb43@googlemail.com>
2018-06-12 00:13:37 +0100

    Create Apollo query to fetch group messages
    Now populating the Messages Screen with data fetched for the group over GraphQL.

--- chatty/client/src/screens/messages.screen.js
+++ chatty/client/src/screens/messages.screen.js
@@ -1,13 +1,12 @@
 import { _ } from 'lodash';
-import {
-  FlatList,
-  StyleSheet,
-  View,
-} from 'react-native';
-import PropTypes from 'prop-types';
+import { ActivityIndicator, FlatList, StyleSheet, View, } from 'react-native';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import randomColor from 'randomcolor';
+import { graphql, compose } from 'react-apollo';
+
 import Message from '../components/message.component';
+import GROUP_QUERY from '../graphql/group.query';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,21 +15,10 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
   },
+  loading: {
+    justifyContent: 'center'
+  }
 });
-const fakeData = () => _.times(100, i => ({
-  // every message will have a different color
-  color: randomColor(),
-  // every 5th message will look like it's from the current user
-  isCurrentUser: i % 5 === 0,
-  message: {
-    id: i,
-    createdAt: new Date().toISOString(),
-    from: {
-      username: `Username ${i}`,
-    },
-    text: `Message ${i}`,
-  },
-}));
 
 class Messages extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -39,21 +27,66 @@ class Messages extends Component {
       title: state.params.title,
     };
   };
-  keyExtractor = item => item.message.id.toString();
-  renderItem = ({ item: { isCurrentUser, message, color } }) => (
+
+  constructor(props) {
+    super(props);
+    const usernameColors = {};
+    if (props.group && props.group.users) {
+      props.group.users.forEach((user) => {
+        usernameColors[user.username] = randomColor();
+      })
+    }
+
+    this.state = {
+      usernameColors
+    };
+
+    this.renderItem = this.renderItem.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const usernameColors = {};
+    // check for new messages
+    if (nextProps.group) {
+      if (nextProps.group.users) {
+        // apply a color for each user
+        nextProps.group.users.forEach((user) => {
+          usernameColors[user.username] = this.state.usernameColors[user.username] || randomColor();
+        });
+      }
+
+      this.setState({
+        usernameColors
+      });
+    }
+  }
+
+  keyExtractor = item => item.id.toString();
+
+  renderItem = ({item: message}) => (
     <Message
-      color={color}
-      isCurrentUser={isCurrentUser}
-      message={message}
-    />
+      color={this.state.usernameColors[message.from.username]}
+      isCurrentUser={message.from.id === 1} // for now until we add auth
+      message={message}/>
   );
 
   render() {
+    const { loading, group } = this.props;
+
+    // render loading placeholder
+    if (loading && !group) {
+      return (
+        <View style={[styles.loading, styles.container]}>
+          <ActivityIndicator/>
+        </View>
+      );
+    }
+
     // render list of messages for group
     return (
       <View style={styles.container}>
         <FlatList
-          data={fakeData()}
+          data={group.messages.slice().reverse()}
           keyExtractor={this.keyExtractor}
           renderItem={this.renderItem}
           ListEmptyComponent={<View/>}
@@ -63,4 +96,25 @@ class Messages extends Component {
   }
 }
 
-export default Messages;
+Messages.propTypes = {
+  group: PropTypes.shape({
+    messages: PropTypes.array,
+    users: PropTypes.array
+  }),
+  loading: PropTypes.bool
+};
+
+const groupQuery = graphql(GROUP_QUERY, {
+  options: ownProps => ({
+    variables: {
+      groupId: ownProps.navigation.state.params.groupId
+    }
+  }),
+  props: ({data: { loading, group }}) => ({
+    loading, group
+  })
+});
+
+export default compose(
+  groupQuery
+)(Messages);
