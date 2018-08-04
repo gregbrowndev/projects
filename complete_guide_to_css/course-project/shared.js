1911d4be929000c3fce740a38309bd4b505074df
Unknown <gregb43@googlemail.com>
2018-08-04 18:51:41 +0100

    Manipulate classList to toggle dynamic elements
    The classList allows us to modify the classes on the element rather than its inline styles.

--- complete_guide_to_css/course-project/shared.js
+++ complete_guide_to_css/course-project/shared.js
@@ -5,8 +5,11 @@ var selectPlanButtons = document.querySelectorAll('.plan button');
 
 for (var i = 0; i < selectPlanButtons.length; ++i) {
     selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = 'open'; // This would override all the classes on modal
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
@@ -16,8 +19,10 @@ modalCancelButton.addEventListener('click', closeModal);
 backdrop.addEventListener('click', closeModal);
 
 function closeModal(){
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    modal.classList.remove('open');
+    backdrop.classList.remove('open');
 }
 
 // Side Navigation
@@ -25,10 +30,14 @@ var toggleButton = document.querySelector('.toggle-button');
 var sideNav = document.querySelector('.mobile-nav');
 
 toggleButton.addEventListener('click', function() {
-    sideNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    // sideNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    sideNav.classList.add('open');
+    backdrop.classList.add('open');
 });
 backdrop.addEventListener('click', function(){
-    sideNav.style.display = 'none';
-    backdrop.style.display = 'none';
+    // sideNav.style.display = 'none';
+    // backdrop.style.display = 'none';
+    sideNav.classList.remove('open');
+    backdrop.classList.remove('open');
 });
