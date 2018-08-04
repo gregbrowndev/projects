7dde65cc68cc296f3287bce5d777912bb58fd663
Unknown <gregb43@googlemail.com>
2018-08-04 19:08:15 +0100

    Tidy up: Make sure side nav works on other screens

--- complete_guide_to_css/course-project/shared.js
+++ complete_guide_to_css/course-project/shared.js
@@ -1,31 +1,5 @@
-// Modal
-var backdrop = document.querySelector('.backdrop');
-var modal = document.querySelector('.modal');
-var selectPlanButtons = document.querySelectorAll('.plan button');
-
-for (var i = 0; i < selectPlanButtons.length; ++i) {
-    selectPlanButtons[i].addEventListener('click', function() {
-        // modal.style.display = 'block';
-        // backdrop.style.display = 'block';
-        // modal.className = 'open'; // This would override all the classes on modal
-        modal.classList.add('open');
-        backdrop.classList.add('open');
-    });
-}
-
-var modalCancelButton = modal.querySelector('.modal__actions .modal__action--negative');
-
-modalCancelButton.addEventListener('click', closeModal);
-backdrop.addEventListener('click', closeModal);
-
-function closeModal(){
-    // modal.style.display = 'none';
-    // backdrop.style.display = 'none';
-    modal.classList.remove('open');
-    backdrop.classList.remove('open');
-}
-
 // Side Navigation
+var backdrop = document.querySelector('.backdrop');
 var toggleButton = document.querySelector('.toggle-button');
 var sideNav = document.querySelector('.mobile-nav');
 
