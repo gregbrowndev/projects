61391652f042bc953b42f3609963f04c3a40e5ec
Unknown <gregb43@googlemail.com>
2018-08-18 19:17:45 +0100

    Add sqlalchemy-repr to generate repr
    This base mixin generates the repr dunder method for your models. This is useful for debugging and avoiding boilerplate.

--- scrapy-bikeshare/db/models.py
+++ scrapy-bikeshare/db/models.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Column, Integer, String, Sequence, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_repr import RepresentableBase
 
-Base = declarative_base()
+Base = declarative_base(cls=RepresentableBase)
 
 
 class System(Base):
@@ -16,18 +17,6 @@ class System(Base):
     url = Column(String)
     language = Column(String)
     created = Column(DateTime, default=func.now())
-
-    def __repr__(self):
-        return f'<System(' \
-               f'source_id=${self.source_id},' \
-               f'name=${self.name}' \
-               f'phone_number=${self.phone_number}' \
-               f'email=${self.email}' \
-               f'timezone=${self.timezone}' \
-               f'url=${self.url}' \
-               f'language=${self.language}' \
-               f'created=${self.created}' \
-               f')>'
 
 
 if __name__ == '__main__':
