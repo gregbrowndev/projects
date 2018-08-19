e93e84e99854474b2b168626345f5f66016eb746
Unknown <gregb43@googlemail.com>
2018-08-19 03:04:41 +0100

    Register models in models/__init__.py
    I've gone back to using the __init__ module to import the SqlAlchemy models. This essentially registers them in the MetaData, so the whole model can be built without having to import everything manually.
    
    This means you cannot cross-import anything from within the models, i.e. cannot import System in stations.py (even for main entry point scripting), as it throws an error.

--- scrapy-bikeshare/db/models/system.py
+++ scrapy-bikeshare/db/models/system.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import relationship
 from db.mixins.scraper_item_mixin import ScraperItemMixin
 from db.mixins.timestamp_mixin import TimestampMixin
 from db.models.base import Base
-from db.utils import create_session, StringNotNullColumn
+from db.utils import StringNotNullColumn
 
 
 class System(ScraperItemMixin, TimestampMixin, Base):
@@ -16,27 +16,3 @@ class System(ScraperItemMixin, TimestampMixin, Base):
     language = StringNotNullColumn()
 
     stations = relationship("Station", back_populates="system")
-
-
-if __name__ == '__main__':
-    session = create_session()
-
-    system = System(
-        name='TfL Bikeshare',
-        phone_number='0343 222 6666',
-        timezone='Europe/London',
-        url='https://tfl.gov.uk/modes/cycling/santander-cycles',
-        language='en')
-
-    try:
-        session.add(system)
-        session.commit()
-
-        # query again
-        obj = session.query(System).first()
-        print(obj)
-    except:
-        session.rollback()
-        raise
-    finally:
-        session.close()
