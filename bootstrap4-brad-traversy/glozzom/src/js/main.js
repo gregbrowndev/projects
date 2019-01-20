13e3eddfc4eee9213908ed7c8fcdb21cacae55b2
Unknown <gregb43@googlemail.com>
2019-01-20 16:24:04 +0000

    Create testimonial section using slick slider

--- bootstrap4-brad-traversy/glozzom/src/js/main.js
+++ bootstrap4-brad-traversy/glozzom/src/js/main.js
@@ -1,15 +1,23 @@
-const bootstrap = require('bootstrap');
-const lightbox = require('ekko-lightbox');
-const slickCarousel = require('slick-carousel');
-
 // Add jQuery to globals
 window.jQuery = require('jquery');
 window.$ = global.jQuery;
+
+const bootstrap = require('bootstrap');
+const lightbox = require('ekko-lightbox');
+const slickCarousel = require('slick-carousel');
 
 // Lightbox Init
 $(document).on('click', '[data-toggle="lightbox"]', function (event) {
   event.preventDefault();
   $(this).ekkoLightbox();
+});
+
+// Initialise slick slider
+// NOTE - this is used in about.html and likely should be inline in the page?
+$('.slider').slick({
+  infinite: true,
+  slidesToScroll: 1,
+  slidesToShow: 1
 });
 
 module.exports = {
