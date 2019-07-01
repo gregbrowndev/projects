a4f5e85e0c691d8fa9fe7fa7ba710c62486116cf
Unknown <gregb43@googlemail.com>
2019-07-01 21:36:22 +0100

    Adding redirect and parsing POST body

--- node/academind_complete_guide/section3-creating-a-node-server/app.js
+++ node/academind_complete_guide/section3-creating-a-node-server/app.js
@@ -1,7 +1,38 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const url = req.url;
+    // process.exit();
+    if (url == '/') {
+        res.write('<html>');
+        res.write('<body><form action="/message/" method="POST"><input type="text" name="message"></input><button type="submit">Submit</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if (url == '/message/' && req.method == 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+            console.log(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        // res.writeHead(302, {'Location': '/'});
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end()
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
