f6c06c659fe46d670eeccb398a7ca7fb3796f589
Greg Brown <greg.brown@itoworld.com>
2020-09-22 21:15:39 +0100

    Add CreatePost component to submit data to PostsService

--- node/microservices-course/blog-app/client/src/App.js
+++ node/microservices-course/blog-app/client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-
+import PostCreate from './PostCreate';
 
 export default () => {
-    return <div>Blog app</div>;
+    return <div className="container">
+        <h1>Create Post</h1>
+        <PostCreate />
+    </div>;
 };
