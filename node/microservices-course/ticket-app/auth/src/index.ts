2ffa005740b01f3ad0000945a2ee6354a46206e9
Greg Brown <gregbrowndev@gmail.com>
2020-11-06 22:30:56 +0000

    Create Ingress config

--- node/microservices-course/ticket-app/auth/src/index.ts
+++ node/microservices-course/ticket-app/auth/src/index.ts
@@ -6,6 +6,11 @@ const PORT = 3000;
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+    res.send("Hi there!");
+});
+
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
