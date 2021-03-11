c7b8c107884c8c408d4c4dc6eb1b6a5c27b3651d
Greg Brown <gregbrowndev@gmail.com>
2021-03-11 22:00:36 +0000

    Add 404 error and fix async error handling

--- node/microservices-course/ticket-app/auth/src/index.ts
+++ node/microservices-course/ticket-app/auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import {json} from 'body-parser';
 
 import {currentUserRouter} from "./routes/current-user";
@@ -6,6 +7,7 @@ import {signinRouter} from "./routes/signin";
 import {signoutRouter} from "./routes/signout";
 import {signupRouter} from "./routes/signup";
 import {errorHandler} from "./middlewares/error-handling";
+import {NotFoundError} from "./errors/not-found-error";
 
 const PORT = 3000;
 
@@ -13,6 +15,11 @@ const app = express();
 app.use(json());
 
 app.use([currentUserRouter, signinRouter, signoutRouter, signupRouter]);
+
+app.all("*", async (req, res, next) => {
+    throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
