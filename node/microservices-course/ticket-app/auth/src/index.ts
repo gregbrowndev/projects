9f7275260dd091b9c27a8f8af7c026d08cc39cbd
Greg Brown <gregbrowndev@gmail.com>
2021-03-12 09:57:55 +0000

    Add MongoDB and mongoose

--- node/microservices-course/ticket-app/auth/src/index.ts
+++ node/microservices-course/ticket-app/auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
+import mongoose from 'mongoose';
 
 import {currentUserRouter} from "./routes/current-user";
 import {signinRouter} from "./routes/signin";
@@ -22,6 +23,21 @@ app.all("*", async (req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-})
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-src:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('Connected to MongoDb');
+    } catch (err) {
+        console.error(err);
+    }
+
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    })
+}
+
+start();
