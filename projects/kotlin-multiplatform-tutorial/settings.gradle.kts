5d477d28bb4eece8ee3d3038a06638ec48796baf
Greg Brown <greg@domainfusion.io>
2023-09-20 18:58:15 +0100

    feat(kmp): Kotlin Multiplatform Tutorial

---
+++ projects/kotlin-multiplatform-tutorial/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KotlinMultiplatformTutorial"
+include(":androidApp")
+include(":shared")
